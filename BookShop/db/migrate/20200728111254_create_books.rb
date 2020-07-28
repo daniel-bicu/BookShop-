b9c5e32e1cce918035686cef4b21769668010a8e
daniel-bicu <danielyoi1233@gmail.com>
2020-07-28 14:33:59 +0300

    initial commit

---
+++ BookShop/db/migrate/20200728111254_create_books.rb
@@ -0,0 +1,10 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :name, :data, :avalaibility
+      t.text :description
+      t.decimal :price, precision: 2, default: 0
+      t.timestamps
+    end
+  end
+end
